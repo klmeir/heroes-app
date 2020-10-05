8c6d37746007e3fc5b8f8be7117d94504aec5a0c
klmeir <caalmeir@gmail.com>
2020-10-05 16:30:39 -0500

    Add CSS animations

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -35,11 +35,11 @@ export const HeroScreen = ({ history }) => {
                 <img 
                     src={ `../assets/heroes/${ heroeId }.jpg` }
                     alt={ superhero }
-                    className="img-thumbnail"
+                    className="img-thumbnail animate__animated animate__fadeInLeft animate__fast"
                 />
             </div>
 
-            <div className="col-8">
+            <div className="col-8 animate__animated animate__fadeIn">
                 <h3> { superhero } </h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-item"> <b> Alter ego: </b> { alter_ego } </li>
